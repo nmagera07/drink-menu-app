3fb55b8723bfe6b62d0b30e7aac99f03eb424652
Nate <nmagera07@gmail.com>
2020-10-08 21:31:41 -0400

    creating main page

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function navbar() {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Drinks</li>
+            </ul>
+        </div>
+    )
+}
